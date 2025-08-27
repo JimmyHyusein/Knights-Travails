6a4128d6ecc9080371046ddccbb41832b5f58340
JimmyHyusein <djimkata12@gmail.com>
2025-08-27 15:07:26 +0300

    Add files via upload

---
+++ knight.js
@@ -0,0 +1,84 @@
+// function knightMoves(x,y){
+//     let moves = [
+//     [2, 1], [2, -1], [-2, 1], [-2, -1],
+//     [1, 2], [1, -2], [-1, 2], [-1, -2]
+//   ];
+
+//   let validMoves = []
+//   for(let [dx, dy] of moves){
+//     let newX = x + dx;
+//     let newY = y + dy;
+//     if(newX <= 7 && newX >= 0 && newY <= 7 && newY >= 0)
+//         validMoves.push([newX, newY]);
+        
+   
+//   }
+//    console.log(validMoves);
+    
+// }
+
+// let queue = [];
+// queue.push("A");
+// queue.push("B");
+// queue.push("C");
+
+// while (queue.length > 0) {
+   
+  
+//   console.log(queue);
+//   let current = queue.shift();
+// }
+
+
+
+
+function isValid(x, y){
+    return x>=0 && x<=7 && y>=0 && y<=7;
+        
+    
+}
+function getMoves(x,y){
+    let moves = [
+     [2, 1], [2, -1], [-2, 1], [-2, -1],
+    [1, 2], [1, -2], [-1, 2], [-1, -2]
+   ];
+    let validMoves = [];
+    for(let [dx, dy] of moves)
+    {
+        let newX = x + dx;
+        let newY = y + dy;
+        if(isValid(newX, newY)) validMoves.push([newX, newY]);
+    }
+    return validMoves;
+}
+function knightMoves(start, end){
+    let queue = [[start]];
+
+    let visited = new Set();
+    visited.add(start.toString());
+    while (queue.length > 0)
+    {
+        let path = queue.shift();
+
+        let current = path[path.length-1];
+        if (current[0] === end[0] && current[1] === end[1])
+            return path;
+        let moves = getMoves(current[0],current[1]);
+        for(let move of moves){
+            if(!visited.has(move.toString())){
+                visited.add(move.toString());
+                queue.push([...path,move]);
+            }
+
+        }
+        
+    }
+    
+}
+console.log(knightMoves([0,0],[7,7]));
+
+let news = [0, 1];
+let move = [1];
+let a   = []
+a.push(...news,move);
+console.log(a);
